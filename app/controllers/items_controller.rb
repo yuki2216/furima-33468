ad198ffd70b8a3443e83723808702ef361e595b8
yuki2216 <76457647+yuki2216@users.noreply.github.com>
2021-02-01 16:27:30 +0900

    active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
   # before_action :authenticate_user!, except: [:index]
   def index
+    @item = Item.new
   end
 
   def new
@@ -9,12 +10,13 @@ class ItemsController < ApplicationController
   end
   
   def create
-    Item.create(item_params)
+    @item = Item.create(item_params)
+    @item.save
   end
 
   private
   def item_params
-    params.require(:item).permit(:title, :user, :description, :category_id, :condition_id, :shipping_user_id, :shipping_area_id, :shipping_day_id, :price)
+    params.require(:item).permit(:title, :user, :description, :category_id, :condition_id, :shipping_user_id, :shipping_area_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
 
 end
