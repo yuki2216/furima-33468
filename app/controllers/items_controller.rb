13daa9a19dcc12e5efc9b2f9a7bc25f18bdbedfe
yuki2216 <76457647+yuki2216@users.noreply.github.com>
2021-02-01 12:12:00 +0900

    itemテーブル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,17 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :user, :description, :category_id, :condition_id, :shipping_user_id, :shipping_area_id, :shipping_day_id, :price)
+  end
+
 end
