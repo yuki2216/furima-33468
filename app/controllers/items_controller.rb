69a934ab4648464012159da1393a38e3c57deec1
yuki2216 <76457647+yuki2216@users.noreply.github.com>
2021-02-03 21:41:00 +0900

    商品編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   
   # before_action :move_to_index, except: [:index, :show]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :item_id, only: [:show, :edit, :update]
   def index
     @items = Item.order("created_at DESC")
   end
@@ -20,12 +21,25 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+  
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :new
+    end
   end
 
   private
   def item_params
     params.require(:item).permit(:title, :image, :description, :category_id, :condition_id, :shipping_user_id, :shipping_area_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
-
+  
+  def item_id
+    @item = Item.find(params[:id])
+  end
 end
