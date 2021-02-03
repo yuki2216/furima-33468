a7dabb5ec1b79e8875814372a11613e79a17a6e6
yuki2216 <76457647+yuki2216@users.noreply.github.com>
2021-02-03 12:52:32 +0900

    Update shipping_day.rb

--- app/models/shipping_day.rb
+++ app/models/shipping_day.rb
@@ -1,8 +1,9 @@
 class ShippingDay < ActiveHash::Base
   self.data = [
     { id: 0, name: '---' },
-    { id: 1, name: '着払い(購入者負担)' },
-    { id: 2, name: '送料込み(出品者負担)' }
+    { id: 1, name: '1~2日で発送' },
+    { id: 2, name: '2~3日で発送' },
+    { id: 3, name: '4~7日で発送' }
   ]
 
   include ActiveHash::Associations
