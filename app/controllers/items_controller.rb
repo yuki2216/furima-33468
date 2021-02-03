af5d95f0a99f3ca3f97c47f2837564f55d5db6c1
yuki2216 <76457647+yuki2216@users.noreply.github.com>
2021-02-03 19:43:05 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   
   # before_action :move_to_index, except: [:index, :show]
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @items = Item.order("created_at DESC")
   end
