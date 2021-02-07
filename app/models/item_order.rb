a679cc5cb5444e8a3a8fb27f302b243b1512c412
yuki2216 <76457647+yuki2216@users.noreply.github.com>
2021-02-07 19:58:38 +0900

    remove_id

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -15,6 +15,6 @@ class ItemOrder
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
-    Address.create!(postal_code: postal_code, prefecture: prefecture, city: city, address: address, building: building, phone_number_id: phone_number, order_id: order.id)
+    Address.create!(postal_code: postal_code, prefecture: prefecture, city: city, address: address, building: building, phone_number: phone_number, order_id: order.id)
   end
 end
